cd46b86237bb23b81c8b2412b378ea275b2bd268
Leeqiqo <s1cho@proton.me>
2022-12-25 23:04:57 +0700

    initial release

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true

6e118471056513738981123ff643ebb00d055afd
SichOo <s1cho@proton.me>
2022-12-27 21:23:01 +0700

    Initial Release

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,14 +1,9 @@
+-- Leader using space
 vim.g.mapleader = ' '
 
--- Fast Escape
+-- Fast to NomalMode
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('i', 'kj', '<esc>')
-
--- Disabled ArrowKeybind
-vim.keymap.set( {'n', 'i', 'v'}, '<Top>', '<nop>')
-vim.keymap.set( {'n', 'i', 'v'}, '<Bottom>', '<nop>')
-vim.keymap.set( {'n', 'i', 'v'}, '<Right>', '<nop>')
-vim.keymap.set( {'n', 'i', 'v'}, '<Left>', '<nop>')
 
 -- File Manager
 vim.keymap.set('n', '<Leader>fm', vim.cmd.Ex)

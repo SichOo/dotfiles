dbd244606067d66975bcab64bd29c6144b33e29f
SichOo <s1cho@proton.me>
2023-01-08 20:03:13 +0700

    minimal config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,26 @@
-require('user')
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = 'yes'
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+vim.opt.splitbelow = false
+vim.opt.splitright = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.smartcase = true
+
+vim.opt.clipboard:append('unnamedplus')
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25

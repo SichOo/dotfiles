bb3ad707d789c2cfad53a9589874e16baa5b38ab
SichOo <s1cho@proton.me>
2023-01-12 16:07:35 +0700

    Initial release

---
+++ .config/nvim/lua/sicho/init.lua
@@ -0,0 +1,6 @@
+require('sicho.settings')
+require('sicho.keymaps')
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ .config/nvim/lua/sicho/keymaps.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<Leader>q', '<cmd>quit<cr>')
+vim.keymap.set('n', '<Leader>w', '<cmd>write<cr>')
+
+vim.keymap.set('n', '<Leader>fm', '<cmd>Ex<cr>')

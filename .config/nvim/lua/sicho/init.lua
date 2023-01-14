d854927d3ca8a917c2d076cc8268fa86e0ab7dc8
SichOo <s1cho@proton.me>
2023-01-14 14:40:41 +0700

    Initial release

--- .config/nvim/lua/sicho/init.lua
+++ .config/nvim/lua/sicho/init.lua
@@ -1,6 +1,33 @@
 require('sicho.settings')
 require('sicho.keymaps')
 
+local augroup = vim.api.nvim_create_augroup
+local sichoGroup = augroup('sicho', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = sichoGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25

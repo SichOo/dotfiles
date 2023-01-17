5b9926312a1638423eea3ca2f4f365224323e348
SichOo <s1cho@proton.me>
2023-01-17 22:25:22 +0700

    Update : zsh-z added

--- .config/zsh/plugins/plugins.zsh
+++ .config/zsh/plugins/plugins.zsh
@@ -19,8 +19,13 @@ if [[ ! -d ${ZDOTDIR}/plugins/zsh-history-substring-search ]]; then
   git clone https://github.com/zsh-users/zsh-history-substring-search.git ${ZDOTDIR}/plugins/zsh-history-substring-search
 fi
 
+if [[ ! -d ${ZDOTDIR}/plugins/zsh-z ]]; then
+  git clone https://github.com/agkozak/zsh-z.git ${ZDOTDIR}/plugins/zsh-z
+fi
+
 . ${ZDOTDIR}/plugins/F-Sy-H/F-Sy-H.plugin.zsh
 . ${ZDOTDIR}/plugins/zsh-autopair/autopair.zsh
 autopair-init
 . ${ZDOTDIR}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 . ${ZDOTDIR}/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+. ${ZDOTDIR}/plugins/zsh-z/zsh-z.plugin.zsh
